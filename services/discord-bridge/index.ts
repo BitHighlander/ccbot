4847f8f7fcd8a8dddd775e8041e483b455db52c9
highlander <bithighlander@gmail.com>
2021-07-21 22:23:08 -0600

    custom emoji work

--- services/discord-bridge/index.ts
+++ services/discord-bridge/index.ts
@@ -1,12 +1,12 @@
 
 /*
-
+    Discord Bridge
         Redis pub/sub
 
 
  */
 // log.infoger function
-const TAG = 'Slack'
+const TAG = 'Discord'
 require('dotenv').config()
 require('dotenv').config({path:"../../.env"});
 require('dotenv').config({path:"./../../.env"});
@@ -20,25 +20,15 @@ const {redis,subscriber,publisher} = require("@pioneer-platform/default-redis")
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const botName = 'cappy'
-const defaultChannelName = "markets"
 const Tokenizer = require('sentence-tokenizer')
 const tokenizer = new Tokenizer('reddit')
 
+let discordChannel = process.env['DISCORD_BOT_CHANNEL']
+let botUserId = process.env['BOT_USER_ID'] || '865670112611008524'
 
-// app on_start
-let usersByIndex:any = {}
-let usersByName:any = {}
-// let userAccounts = {}
-
-let params = {
-  icon_emoji: ':coincap_v2:',
-}
-
-let tag = " | slackbot | "
 let msg:any
+if(!process.env['DISCORD_BOT_TOKEN']) throw Error("env DISCORD_BOT_TOKEN required!")
 bot.login(process.env['DISCORD_BOT_TOKEN']);
-
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
@@ -48,8 +38,9 @@ bot.on('ready', () => {
 // subscribe to redis
 subscriber.subscribe('publish')
 subscriber.on('message', async function (channel:any, payloadS:string) {
+  let tag = TAG + ' message '
   try {
-    if(msg.author && msg.author.id !== '865670112611008524'){
+    if(msg.author && msg.author.id !== '865670112611008524' && msg.author.id !== botUserId){
       let payload = JSON.parse(payloadS)
       log.info('payload: ', payload)
       log.info('msg author id: ', msg.author)
@@ -59,22 +50,99 @@ subscriber.on('message', async function (channel:any, payloadS:string) {
       if (!payload.view) throw Error('101: invalid payload missing: view')
 
       log.info('msg.channel.name: ', msg.channel.name)
-      if(msg.channel.name === '📈markets' || msg.channel.name === 'markets'){
+      if(msg.channel.name === '📈markets' || msg.channel.name === 'markets' || discordChannel){
         log.info(tag,"WINNING!: ")
         log.info(tag,"msg.emojis",msg.emojis)
+
         //let emojiRef = "\:btc:"
         // let emojiRef = msg.guild.emojis.cache.find((emoji: { name: string; }) => emoji.name === 'btc');
+        // log.info(tag,"**** emojiRef: ",emojiRef)
         // emojiRef = "<:btc:595760361110634496>"
         // log.info(tag,"emojiRef: ",emojiRef)
 
-        if(payload.view.text){
-          msg.channel.send(payload.view.text);
+        //if message display it
+        if(payload.msg){
+          let publish = []
+          log.info("Publish View: ")
+          tokenizer.setEntry(payload.msg);
+          //replace emojis and reformat
+          const sentences = tokenizer.getSentences()
+          log.info(tag,"sentences: ",sentences)
+          const tokens = tokenizer.getTokens(sentences)
+          log.info(tag,"tokens: ",tokens)
+
+          for(let i = 0; i < tokens.length; i++){
+            let token = tokens[i]
+            log.info(tag,"token: ",token)
+
+            //if has :
+            if(token.indexOf(":") >= 0){
+              //strip emoji
+              let tokenSplit = token.split(':')
+              log.info(tag,"tokenSplit: ",tokenSplit)
+              // let pop = tokenSplit.pop()
+              // log.info(tag,"pop: ",pop)
+              // let tokenSplit2 = pop.split(':')
+              // log.info(tag,"tokenSplit2: ",tokenSplit2)
+              let emojiString = tokenSplit[1]
+
+
+              // let emojiString = token.split(':').pop().split(':')[0]; // returns 'two'
+              log.info(tag,"emojiString: ",emojiString)
+
+              //lookup magic id
+              let emojiId = await msg.guild.emojis.cache.find((emoji: { name: string; }) => emoji.name === emojiString);
+              log.info(tag,"emojiId: ",emojiId)
+
+              if(!emojiId){
+                publish.push(token)
+              }else{
+                //replace in token
+                let emojiDiscord = "<:"+emojiString+":"+emojiId+">"
+                let tokenTranslated = token.replace(emojiString,emojiDiscord)
+                publish.push(tokenTranslated)
+              }
+            } else {
+              publish.push(token)
+            }
+          }
+
+          const search = ',';
+          const searchRegExp = new RegExp(search, 'g'); // Throws SyntaxError
+          const replaceWith = ' ';
+          msg.channel.send(publish.toString().replace(searchRegExp, replaceWith));
         }
+
+
+        if(payload.view.text){
+          let publish = []
+          //TODO
+          log.info("Publish View: TEXT")
+
+          // msg.channel.send(publish.toString());
+        }
+
         if(payload.view.attachments){
           log.info("attachments: ",payload.view.attachments)
-          msg.channel.send(payload.msg);
+          // msg.channel.send(payload.msg);
           if(payload.view.attachments[0].image_url){
             msg.channel.send(payload.view.attachments[0].image_url);
+
+            // const attachment = new Discord.MessageEmbed()
+            //     .setColor("#0099ff")
+            //     .setAuthor(
+            //         "ccBot",
+            //         "payload.view.attachments[0].image_url",
+            //         "https://coincap.io/assets/fox-token"
+            //     )
+            //     .addFields(
+            //         { name: "Price", value: "$0.70", inline: true },
+            //         { name: "Change", value: "-8.00% 📈", inline: true, setColor: '#0099ff' },
+            //     )
+            //     .setTimestamp()
+            //     .setFooter("CoinCap", "https://iconape.com/wp-content/png_logo_vector/coincap.png");
+            // msg.channel.send(attachment);
+
           }
         } else {
           log.error('Wrong channel: ',msg.channel.name)
